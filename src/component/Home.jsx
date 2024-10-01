81aa3863378de4b8e18409e895ab9fb85425b608
Prem <prem.gujel@ibytinfomatics.in>
2024-10-01 22:27:56 +0530

    update home file

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
             <img className="rounded-5 w-100" src="images/pexels-gantas.jpg" />
             </div>
             <div className="col-md-6">
-            <h2 className="mt-5 display-6 pb-4">About SpotGuard.AE</h2>
+            <h2 className="mt-5 display-6 pb-4 text-center">About SpotGuard.AE</h2>
             <p className="text-center">Premier cleaning in the UAE for residential and commercial spaces across Dubai, Abu Dhabi, and Sharjah. Our expert team delivers spotless, eco-friendly results with cutting-edge equipment at competitive rates. Contact us for a free quote and experience the SpotGuard difference!</p>
             </div>
         </div>
@@ -46,35 +46,35 @@ const Home = () => {
         </div>
         <div className="container">
              <div className="pt-4">
-                 <h1 className="display-6 text-centerl fw-bold pb-4">Why Choose SpotGuard for Residential Cleaning in Dubai?</h1>
+                 <h1 className="fw-bold pb-4">Why Choose SpotGuard for Residential Cleaning in Dubai?</h1>
              </div>
-       <div className="d-flex pb-2">
-      <span> <IoIosArrowForward className="mt-1 mr-2"/></span>
-        <p className="pd-3">Expert Team:Our cleaners are highly trained, experienced, and dedicated to delivering exceptional results.</p>
+       <div className="d-flex py-2">
+      <span> <IoIosArrowForward className="mt-1 mr-2 fw-bold"/></span>
+        <p className="">Expert Team:Our cleaners are highly trained, experienced, and dedicated to delivering exceptional results.</p>
         </div>
-        <div className="d-flex pb-2">
-        <spn><IoIosArrowForward className="mr-2 mt-1"/></spn>
+        <div className="d-flex py-2">
+        <spn><IoIosArrowForward className="mr-2 mt-1 fw-bold"/></spn>
         <p className="">Eco-Friendly Products: We use environmentally friendly cleaning products that are safe for your family and pets.</p>
         </div>
-        <div className="d-flex pb-2">
+        <div className="d-flex py-2">
             <span>
-            <IoIosArrowForward className="mr-2 mt-1"/>
+            <IoIosArrowForward className="mr-2 mt-1 fw-bold"/>
             </span>
         <p className=""> Flexible Scheduling: We offer convenient scheduling options to fit your busy lifestyle.</p>
         </div>
-        <div className="d-flex pb-2">
-        <span><IoIosArrowForward className="mr-2 mt-1"/></span>
+        <div className="d-flex py-2">
+        <span><IoIosArrowForward className="mr-2 mt-1 fw-bold"/></span>
         <p className=""> Competitive Pricing: Our services are competitively priced to provide excellent value without compromising on quality.</p>
         </div>
-        <div className="d-flex pb-2">
-        <span><IoIosArrowForward className="mr-2 mt-1"/></span>
+        <div className="d-flex py-2">
+        <span><IoIosArrowForward className="mr-2 mt-1 "/></span>
         <p> Customer Satisfaction: Your satisfaction is our top priority. We strive to exceed your expectations with every clean.</p>
         </div>
 
 </div>
 
 
-<div className="pt-5 ">
+<div className="py-5 ">
     <img src = "images/New-Apartment.jpg"></img>
 </div>
 <div className="container">
