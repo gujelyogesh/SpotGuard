5838a3d50911b71a08316e403dc7243cabdea78e
Prem <prem.gujel@ibytinfomatics.in>
2024-10-02 09:46:33 +0530

    update ourservice file

---
+++ src/component/OurService.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+const OurService = () =>{
+    return(
+        <>
+        <Header/>
+        <h1>this is ourService</h1>
+        <Footer />
+        </>
+    )
+}
+
+export default OurService;
