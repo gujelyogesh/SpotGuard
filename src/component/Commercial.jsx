4e8dd998fdd8801f010fae86355140084047ff81
Prem <prem.gujel@ibytinfomatics.in>
2024-10-02 09:43:10 +0530

    update commercial file

---
+++ src/component/Commercial.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Commercial = () =>{
+    return(
+        <>
+        <Header/>
+        <h1>this is commercial</h1>
+        <Footer />
+        </>
+    )
+}
+
+export default Commercial;
