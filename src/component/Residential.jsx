cd6071badd1848123de24cff556ef33bbe5b83e8
Prem <prem.gujel@ibytinfomatics.in>
2024-10-02 09:40:26 +0530

    update Residential file

---
+++ src/component/Residential.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Residential = () =>{
+    return(
+        <>
+        <Header/>
+        <h1>this is residential</h1>
+        <Footer />
+        </>
+    )
+}
+
+export default Residential;
